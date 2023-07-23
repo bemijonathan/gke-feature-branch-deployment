d626049011772b1fc6ddd4286faf2ad5746e58ea
Jonathan Atiene <34762800+bemijonathan@users.noreply.github.com>
2023-07-23 17:12:10 +0100

    deployed backend to GKE

--- backend/index.ts
+++ backend/index.ts
@@ -4,6 +4,6 @@ import { App } from './server'
 // instantiate all environment variables
 dotenv.config()
 
-const server = new App(Number(process.env.PORT) || 3000, process.env.DATABASE_URI || '')
+const server = new App(Number(process.env.PORT) || 3000, process.env.DATABASE_URL || '')
 
 server.startServer()
